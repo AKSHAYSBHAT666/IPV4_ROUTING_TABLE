95fa77c646b65217bb91e0c0acc0c6a27dceb0b5
AKSHAYSBHAT666 <akshaybhat456@gmail.com>
2022-11-04 15:39:46 +0530

    lookup's have been handled

--- 1.c
+++ 1.c
@@ -8,24 +8,25 @@
 //01111011100000000101000000001001
 //123.128.80.9/10 
 //128.128.128.128/32
-//128.2.0.0/3
-//128.2.4.0/4
-
-
+/*
+128.2.0.0/3
+128.2.4.0/4
+128.2.4.5/3
+*/
 typedef struct NODE mtrie_node;
 typedef struct TABLE table;
 typedef enum bit_type {ZERO,ONE,DONT_CARE,BIT_TYPE_MAX} BIT_TYPE;
 
 struct NODE
 {
-	int prefix[32];
-	int wildcard[32];
-	int prefix_len;
-	int starting;
-	int ending;
+	uint8_t prefix[32];
+	uint8_t wildcard[32];
+	uint8_t prefix_len;
+	uint8_t starting;
+	uint8_t ending;
 	
 	mtrie_node* parent;
-	mtrie_node* child[3];
+	mtrie_node* child[BIT_TYPE_MAX];
 };
 
 struct TABLE
@@ -33,6 +34,7 @@ struct TABLE
 	mtrie_node mroot;
 	//function pointer's to interface each of them.
 	void	(*fptr_insert)(table*,char*);
+	int		(*fptr_lookup)(table*,char*);
 };
 
 
@@ -42,7 +44,7 @@ int create(table*tt,mtrie_node*mn,int index)
 	{
 		// if node is not present, insert into the specified index.
 		(tt->mroot).child[index]=mn;
-		return 1;
+		return ONE;
 	}
 	else
 	{
@@ -90,16 +92,15 @@ int create(table*tt,mtrie_node*mn,int index)
 						temp->child[!(mn->prefix[start])]=daughter;
 					}
 					else free(mn);//if already exists, then free the node.
-					return 1;
+					return ONE;
 				}
 			}
 			if(start<32) temp=temp->child[mn->prefix[start]];//continue to next node.
-			else {free(mn);return 0;}
+			else {free(mn);return ZERO;}
 		}
-		return 0;
+		return ZERO;
 	}
 }
-
 
 void insert(table*tt,char*str)
 {
@@ -122,8 +123,8 @@ void insert(table*tt,char*str)
 			sum=sum/10;
 			for(int k=0;k<8;k++)
 			{
-				if(sum & (1<<(k)))	mnode->prefix[7-k+shift]=1;
-				else 				mnode->prefix[7-k+shift]=0;
+				if(sum & (1<<(k)))	mnode->prefix[7-k+shift]=ONE;
+				else 				mnode->prefix[7-k+shift]=ZERO;
 			}
 			shift+=8;
 			sum=0;
@@ -135,8 +136,8 @@ void insert(table*tt,char*str)
 			sum=sum/10;
 			for(int k=0;k<8;k++)
 			{
-				if(sum & (1<<(k)))	mnode->prefix[7-k+shift]=1;
-				else 				mnode->prefix[7-k+shift]=0;
+				if(sum & (1<<(k)))	mnode->prefix[7-k+shift]=ONE;
+				else 				mnode->prefix[7-k+shift]=ZERO;
 			}
 			sum=0;
 			
@@ -153,40 +154,153 @@ void insert(table*tt,char*str)
 		}
 	}
 	// processing of ip completed, proceed to insertion.
-	if(mnode->prefix[0]==0)
+	if(mnode->prefix[ZERO]==ZERO)
 	{
-		printf("%d",create(tt,mnode,0));
+		printf("\n%d",create(tt,mnode,ZERO));
 	}
 	else
 	{
-		printf("%d",create(tt,mnode,1));
+		printf("\n%d",create(tt,mnode,ONE));
 	}
 };
+
+int find(table*tt,mtrie_node*mn,int index)
+{
+	if((tt->mroot).child[index]==NULL)
+	{
+		// if node is not present
+		return ZERO;
+	}
+	else
+	{
+		mtrie_node*temp=(tt->mroot).child[index];
+		int start=temp->starting;
+		while(temp!=NULL)
+		{
+			for(start=(temp->starting);start<(temp->ending);start++)
+			{
+				if((temp->prefix[start])!=mn->prefix[start])//point of ip mismatch.
+				{
+					return ZERO;
+				}
+			}
+			if(start<32) temp=temp->child[mn->prefix[start]];//continue to next node.
+			else return ONE;
+		}
+		return ONE;
+	}
+}
+
+int lookup(table*tt,char*str)
+{
+	int sum=0;
+	int shift=0;
+	
+	mtrie_node*mnode=malloc(sizeof(mtrie_node));//node holding current ip;
+	mnode->parent=NULL;
+	for(int i=0;i<3;i++) mnode->child[i]=NULL;
+	
+	//converting ip into prefix.
+	for(int i=0;i<100 && str[i]!='\0';i++)
+	{
+		sum*=10;
+		sum+=str[i]-'0';
+		if(str[i]=='.')
+		{
+			// to get the 32-bit representation.
+			sum-=str[i]-'0';
+			sum=sum/10;
+			for(int k=0;k<8;k++)
+			{
+				if(sum & (1<<(k)))	mnode->prefix[7-k+shift]=ONE;
+				else 				mnode->prefix[7-k+shift]=ZERO;
+			}
+			shift+=8;
+			sum=0;
+		}
+		else if(str[i]=='/')
+		{
+			//to calculate wildcard and subnet-mask.
+			sum-=str[i]-'0';
+			sum=sum/10;
+			for(int k=0;k<8;k++)
+			{
+				if(sum & (1<<(k)))	mnode->prefix[7-k+shift]=ONE;
+				else 				mnode->prefix[7-k+shift]=ZERO;
+			}
+			sum=0;
+			
+			for(int j=i+1;j<100 && str[j]!='\0';j++)
+			{
+				sum*=10;
+				sum+=str[j]-'0';
+			}
+			mnode->prefix_len=32;
+			mnode->starting=0;
+			mnode->ending=32;
+			for(int k=0;k<sum;k++) mnode->wildcard[k]=2;
+			sum=0;
+			break;
+		}
+	}
+	// processing of ip completed, proceed to insertion.
+
+	if(mnode->prefix[ZERO]==ZERO)
+	{
+		sum=find(tt,mnode,ZERO);
+	}
+	else
+	{
+		sum=find(tt,mnode,ONE);
+	}
+	free(mnode);
+	return sum;
+}
 
 void initialise(table*tt)
 {
 	(tt->mroot).parent=NULL;
 	for(int i=0;i<3;i++) (tt->mroot).child[i]=NULL;
 	tt->fptr_insert=insert;
+	tt->fptr_lookup=lookup;
 }
 
 int main()
 {
 			create_table(r1);
 			
-			char *str=malloc(sizeof(char)*50);
+			char *str=malloc(sizeof(char)*30);
 			scanf("%s",str);
 			r1.fptr_insert(&r1,str);
 			printf("\n");
-			char *str1=malloc(sizeof(char)*50);
+			char *str1=malloc(sizeof(char)*30);
 			scanf("%s",str1);
 			r1.fptr_insert(&r1,str1);
 			printf("\n");
-			char *str2=malloc(sizeof(char)*50);
+			char *str2=malloc(sizeof(char)*30);
 			scanf("%s",str2);
 			r1.fptr_insert(&r1,str2);
 			printf("\n");
+			char *str3=malloc(sizeof(char)*30);
+			scanf("%s",str3);
+			printf("\nverdict:%d",r1.fptr_lookup(&r1,str3));
+			printf("\n");
 			/*int cnt=0;
+			for(int i=0;i<32;i++)
+			{
+				if(cnt==8) printf(" "),cnt=0;
+				cnt++;
+				printf("%d",(r1.mroot.child[1])->child[1]->child[0]->prefix[i]);
+			}
+			printf("\n");
+			cnt=0;
+			for(int i=0;i<32;i++)
+			{
+				if(cnt==8) printf(" "),cnt=0;
+				cnt++;
+				printf("%d",(r1.mroot.child[1])->child[1]->child[1]->prefix[i]);
+			}
+			printf("\n");
 			for(int i=0;i<32;i++)
 			{
 				if(cnt==8) printf(" "),cnt=0;
